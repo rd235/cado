a3c151d00be3c7cfd4aeadb1777857715122e633
Renzo Davoli <renzo@cs.unibo.it>
2025-01-08 16:07:56 +0100

    protection against DoS in case of code injection

--- PostInstall.cmake
+++ PostInstall.cmake
@@ -21,7 +21,7 @@ execute_process(COMMAND bash -euc "\
 	ERROR_QUIET OUTPUT_QUIET)
 execute_process(COMMAND mkdir -p ${CADO_SPOOL_DIR} ERROR_QUIET OUTPUT_QUIET)
 execute_process(COMMAND chown root:_cado ${CADO_SPOOL_DIR} ERROR_QUIET OUTPUT_QUIET)
-execute_process(COMMAND chmod 4770 ${CADO_SPOOL_DIR} ERROR_QUIET OUTPUT_QUIET)
+execute_process(COMMAND chmod 1770 ${CADO_SPOOL_DIR} ERROR_QUIET OUTPUT_QUIET)
 execute_process(COMMAND chown :_cado ${BINDIR}/scado ERROR_QUIET OUTPUT_QUIET)
 execute_process(COMMAND chmod g+s ${BINDIR}/scado ERROR_QUIET OUTPUT_QUIET)
 execute_process(COMMAND chown _cado: ${BINDIR}/cado ERROR_QUIET OUTPUT_QUIET)
